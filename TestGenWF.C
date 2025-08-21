776809eca662b247336674d8a8d81e03fece3594
Federico Sanchez <39830777+FedericoSanchezNieto@users.noreply.github.com>
2025-08-21 15:28:21 +0200

    Update TestGenWF.C
    Adapt to the Changes at the simulation level, mainly to get the two (correlated) waveforms from a single event simulation.

--- TestGenWF.C
+++ TestGenWF.C
@@ -23,18 +23,20 @@ int main(int argc, char **argv){
   double time0 = 0; // This is the time of the first sample in the sample array 
 
   // Generate photons at a distance Ll from the sensor, for an "energy", returning the time of the first cell and the content of the cell.  
-  
-  std::vector<double> wfdata = gwf.GeneratefromdEdx(Ll,energy,time0); // Returns the signal in each of the samples.
 
-  double time1 =0; 
-  // This will be the other end of the bar 
-  std::vector<double> wfdata2 = gwf.GeneratefromdEdx(L-Ll,energy,time1); // Returns the signal in each of the samples. 
+  wf.GeneratefromdEdx(Ll,energy);
+
+
+  std::vector<double> wfdata_barend0 = gwf.GetWaveForm(0); // Returns the signal in each of the samples.
+  double time0 = gwf.GetCell0Time(0);
+  std::vector<double> wfdata_barend1 = gwf.GetWaveForm(1); // Returns the signal in each of the samples.
+  double time1 = gwf.GetCell0Time(1);
 
   std::cout << " Time 0 " << time0 << "  " << time1 << std::endl;
   
   for(int i =0 ; i < wfdata.size(); i++ ) {
     
-    std::cout << "Sample["<<i<<"]= \t" << wfdata[i] << "   \t" << wfdata2[i] << std::endl; 
+    std::cout << "Sample["<<i<<"]= \t" << wfdata_barend0[i] << "   \t" << wfdata_barend1[i] << std::endl; 
     
   }
 
